8f0642adf351292d78721f428404d4e936ecaacd
Niblet91 <chris.lawson1605@gmail.com>
2023-05-19 15:32:03 +1000

    fire wand complete

--- Assets/Chris/FireballWand.cs
+++ Assets/Chris/FireballWand.cs
@@ -6,17 +6,41 @@ public class FireballWand : MonoBehaviour
     public GameObject fireballPrefab;        // The fireball prefab to be instantiated
     public Transform fireballSpawnPoint;     // The point at which the fireball will spawn
     public float fireballSpeed = 20f;        // The speed at which the fireball will travel
+    public float interactRadius = 0.5f;      // The radius to check for objects in contact with the wand
+
+    public GameObject leftHandDetection;     // The GameObject representing the left hand detection point
+    public GameObject rightHandDetection;    // The GameObject representing the right hand detection point
 
     private bool canShoot = true;            // Add a boolean flag to indicate if the player can shoot a fireball
     private float fireballCooldown = 0.5f;   // Add a float to set the cooldown time in seconds
 
     void Update()
     {
-        // Check if the index trigger on either Oculus Quest 2 controller is pressed
-        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) || OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+        // Check if the left index trigger is pressed while the controller is in contact with the left hand detection point
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) && IsColliding(leftHandDetection))
         {
             ShootFireball();
         }
+
+        // Check if the right index trigger is pressed while the controller is in contact with the right hand detection point
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger) && IsColliding(rightHandDetection))
+        {
+            ShootFireball();
+        }
+    }
+
+    bool IsColliding(GameObject detectionPoint)
+    {
+        // Check if the detection point's collider is colliding with any other colliders
+        Collider[] colliders = detectionPoint.GetComponents<Collider>();
+        foreach (Collider collider in colliders)
+        {
+            if (collider.bounds.Intersects(GetComponent<Collider>().bounds))
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     void ShootFireball()
@@ -28,7 +52,7 @@ public class FireballWand : MonoBehaviour
 
             // Instantiate the fireball at the spawn point, with the desired scale
             GameObject fireball = Instantiate(fireballPrefab, fireballSpawnPoint.position, fireballSpawnPoint.rotation);
-            //fireball.transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+            fireball.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
 
             // Add physics to the fireball
             Rigidbody fireballRigidbody = fireball.GetComponent<Rigidbody>();
